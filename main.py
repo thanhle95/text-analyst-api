3895d10d987a2f4b604da0972b4b99816b8fb991
Thành Lê <thanhle01101995@gmail.com>
2021-07-01 04:50:08 +0000

    Added Application Insights Logging

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import logging
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+
 import utils
 
 app = FastAPI()
+
+logger = logging.getLogger(__name__)
+logger.setLevel(10)
+logger.addHandler(AzureLogHandler(connection_string='InstrumentationKey=297da27a-656e-4e42-92be-e3d802df65d5'))
+
 
 headers = {
     "Ocp-Apim-Subscription-Key": "b31364ab188641f78ac41a9678a3dd18",
@@ -17,12 +25,24 @@ class Model(BaseModel):
 def analyze_text(text: Model):
     response = {"sentiment": [], "keyphrases": []}
     no_of_text = len(text.text_to_analyze)
+    tasks = []
     for i in range(no_of_text):
         document = {"documents": [{"id": i+1, "language": "en", "text": text.text_to_analyze[i]}]}
 
         sentiment = utils.call_text_analytics_api(headers, document, endpoint='sentiment')
         keyphrases = utils.call_text_analytics_api(headers, document, endpoint='keyPhrases')
         
+
+        log_data = {
+            "custom_dimensions":
+            {
+                "text": text.text_to_analyze[i],
+                "text_sentiment": sentiment["documents"][0]["sentiment"],
+                "text_keyphrases": keyphrases["documents"][0]["keyPhrases"]
+            }
+        }
+        logger.info('Text Processed Succesfully', extra=log_data)
+
         response["sentiment"].append(sentiment["documents"][0])
         response["keyphrases"].append(keyphrases["documents"][0])
 
